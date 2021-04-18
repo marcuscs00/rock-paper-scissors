81c6e59605cda45afcd1fe62867b263f88048de3
Marcus Smolenski <marcussmolenski00@gmail.com>
2021-04-18 13:21:51 -0400

    Made index.html actual rock, paper, scissors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function addTodo() {
-	let todoText = document.getElementById('todo-text');
-	let todoSpace = document.getElementById('todo-space');
-	todoSpace.innerHTML = `<div class="todo-item">${todoText}</div>`
+function rock() {
 }
 
+function paper() {
+}
+
+function scissors() {
+}
 
