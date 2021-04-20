dacf548094cc6a7978f48b34740f711cbfaf4714
Marcus Smolenski <marcussmolenski00@gmail.com>
2021-04-20 06:26:49 -0400

    Added numberProcedure()

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-function submit(c) {
-	opponentsChoice =;
-	if (c === 'paper' && opponentsChoice === 'rock') {
-		// Declare user winner
-	}
-
-	if (opponentsChoice === 'paper' && c === 'rock') {
-		// Declare opponent winner
-	}
-
+function numberProcedure() {
 	if (c > opponentsChoice) {
 		// Declare user winner;
 	}
@@ -15,6 +6,25 @@ function submit(c) {
 	if (c < opponentsChoice) {
 		// Declare opponent winner;
 	}
+}
+
+function submit(c) {
+	opponentsChoice = // Select a random selection from an array
+
+	if (c === 'paper' && opponentsChoice === 'rock') {
+		// Declare user winner
+	} else {
+		numberProcedure();
+	}
+
+	if (opponentsChoice === 'paper' && c === 'rock') {
+		// Declare opponent winner
+	} else {
+		numberProcedure();
+	}
+
+
+	
 
 }
 
