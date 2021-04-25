91b0792e908a02a8afcb42efd23c412dc550520c
Marcus Smolenski <marcussmolenski00@gmail.com>
2021-04-25 06:31:31 -0400

    Added indexes for the choices

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-let opSpace = getElementById('op-space');
-let resultSpace = getElementById('result-space');
+let choiceArray = [[0, 'paper'], [1, 'scissors'], [2, 'rock']];
+
+let opSpace = document.getElementById('op-space');
+let resultSpace = document.getElementById('result-space');
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 
 function numberProcedure() {
-	if (c > opponentsChoice) {
+	// TODO: put somewhere else to account for c;
+	if (c[0] > opponentsChoice[0]) {
 		resultSpace.innerHTML = `${c} won`;
 	}
 
-	if (c < opponentsChoice) {
+	if (c[0] < opponentsChoice[0]) {
 		resultSpace.innerHTML = `${opponentsChoice} won`;
 	}
 }
 
 function submit(c) {
-	let opponentsChoice = getRndInteger(0, 2);
+	let opponentsNum = getRndInteger(0, 2);
+	let opponentsChoice = choiceArray[opponentsNum];
 
-	if (c === 'paper' && opponentsChoice === 'rock') {
+	if (c[1] === 'paper' && opponentsChoice[1] === 'rock') {
 		resultSpace.innerHTML = `${c} won`;
 	} else {
 		numberProcedure();
 	}
 
-	if (opponentsChoice === 'paper' && c === 'rock') {
+	if (opponentsChoice[1] === 'paper' && c[1] === 'rock') {
 		resultSpace.innerHTML = `${opponentsChoice} won`;
 	} else {
 		numberProcedure();
