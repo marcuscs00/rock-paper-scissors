e84b464123c11357a326cef7da0ba4ad5a60f66e
Marcus Smolenski <marcussmolenski00@gmail.com>
2021-04-25 06:36:45 -0400

    Got rock-paper-scissors actually working, and deleted the submit button

--- index.js
+++ index.js
@@ -7,29 +7,32 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 
+function submit(c) {
 function numberProcedure() {
 	// TODO: put somewhere else to account for c;
 	if (c[0] > opponentsChoice[0]) {
-		resultSpace.innerHTML = `${c} won`;
+		resultSpace.innerHTML = `${c[1]} won`;
 	}
 
 	if (c[0] < opponentsChoice[0]) {
-		resultSpace.innerHTML = `${opponentsChoice} won`;
+		resultSpace.innerHTML = `${opponentsChoice[1]} won`;
 	}
 }
 
-function submit(c) {
+
 	let opponentsNum = getRndInteger(0, 2);
 	let opponentsChoice = choiceArray[opponentsNum];
 
+	opSpace.innerHTML = `opponent chose ${opponentsChoice[1]}`;
+
 	if (c[1] === 'paper' && opponentsChoice[1] === 'rock') {
-		resultSpace.innerHTML = `${c} won`;
+		resultSpace.innerHTML = `${c[1]} won`;
 	} else {
 		numberProcedure();
 	}
 
 	if (opponentsChoice[1] === 'paper' && c[1] === 'rock') {
-		resultSpace.innerHTML = `${opponentsChoice} won`;
+		resultSpace.innerHTML = `${opponentsChoice[1]} won`;
 	} else {
 		numberProcedure();
 	}
